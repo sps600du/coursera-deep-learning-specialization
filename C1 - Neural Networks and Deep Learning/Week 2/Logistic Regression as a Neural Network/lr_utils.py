2640a3128d6df49401739f5fe445a60978d09c86
sps600du <67948601+sps600du@users.noreply.github.com>
2023-06-01 11:12:55 -0700

    Update lr_utils.py

--- C1 - Neural Networks and Deep Learning/Week 2/Logistic Regression as a Neural Network/lr_utils.py
+++ C1 - Neural Networks and Deep Learning/Week 2/Logistic Regression as a Neural Network/lr_utils.py
@@ -12,6 +12,7 @@ def load_dataset():
     test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
 
     classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    # for this particular program, it is just coded as 1-"cat", 0-"non-cat"
     
     train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
     test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
